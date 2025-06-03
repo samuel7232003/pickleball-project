591fd25973896c76c4afe44078ab97ebfaab977e
Le Viet Thanh <115569080+samuel7232003@users.noreply.github.com>
2025-06-03 22:05:39 +0700

    feat(payment): test link payment 3

--- backend/controllers/payosController.js
+++ backend/controllers/payosController.js
@@ -1,4 +1,5 @@
 const { setDoneInvoiceService } = require("../services/invoiceService");
+const crypto = require("crypto");
 
 const createPaymaentUrl = async(req, res)=>{
   const { userId, invoiceId, amount, name } = req.body;
@@ -64,4 +65,41 @@ const onStatusPayment = async (req, res) =>{
 
 module.exports ={
   createPaymaentUrl, onStatusPayment
+}
+
+function sortObjDataByKey(object) {
+  const orderedObject = Object.keys(object)
+      .sort()
+      .reduce((obj, key) => {
+          obj[key] = object[key];
+          return obj;
+      }, {});
+  return orderedObject;
+}
+
+function convertObjToQueryStr(object) {
+  return Object.keys(object)
+      .filter((key) => object[key] !== undefined)
+      .map((key) => {
+          let value = object[key];
+          // Sort nested object
+          if (value && Array.isArray(value)) {
+              value = JSON.stringify(value.map((val) => sortObjDataByKey(val)));
+          }
+          // Set empty string if null
+          if ([null, undefined, "undefined", "null"].includes(value)) {
+              value = "";
+          }
+          return `${key}=${value}`;
+      })
+      .join("&");
+}
+
+function isValidData(data, currentSignature, checksumKey) {
+  const sortedDataByKey = sortObjDataByKey(data);
+  const dataQueryStr = convertObjToQueryStr(sortedDataByKey);
+  const dataToSignature = crypto.createHmac("sha256", checksumKey)
+      .update(dataQueryStr)
+      .digest("hex");
+  return dataToSignature == currentSignature;
 }
